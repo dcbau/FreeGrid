98e204244dc0d86209d3f0f1db375b6399283080
dcbau <david_bau@gmx.de>
2021-04-15 17:36:22 +0200

    Bugfixes

--- measurement_controller.py
+++ measurement_controller.py
@@ -257,6 +257,7 @@ class MeasurementController:
 
         else:
             self.measurement.play_sound(False)
+Bu            self.measurement_running_flag = False
 
     def save_to_file(self):
 
